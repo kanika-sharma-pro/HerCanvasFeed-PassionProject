2fba223795129b93d5bae81f13ee33a712e7d636
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-04-21 15:28:29 -0400

    creating UserTest

---
+++ HerCanvasFeed/src/test/java/rocks/zipcode/accessingdatamysql/UserTest.java
@@ -0,0 +1,57 @@
+package rocks.zipcode.accessingdatamysql;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getId() {
+
+    }
+
+    @Test
+    void setId() {
+
+    }
+
+    @Test
+    void getUserName() {
+
+    }
+
+    @Test
+    void setUserName() {
+
+    }
+
+    @Test
+    void getUserEmail() {
+
+    }
+
+    @Test
+    void setUserEmail() {
+
+    }
+
+    @Test
+    void getFirstName() {
+
+    }
+
+    @Test
+    void setFirstName() {
+
+    }
+
+    @Test
+    void getLastName() {
+
+    }
+
+    @Test
+    void setLastName() {
+    }
+}
