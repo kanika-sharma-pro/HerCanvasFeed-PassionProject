f50e3c82798e4933410be27776f9af88b0f36cc6
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-04-21 16:59:53 -0400

    working

--- HerCanvasFeed/src/main/java/rocks/zipcode/accessingdatamysql/User.java
+++ HerCanvasFeed/src/main/java/rocks/zipcode/accessingdatamysql/User.java
@@ -1,18 +1,27 @@
 package rocks.zipcode.accessingdatamysql;
+
 import jakarta.persistence.*;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 @Entity
 public class User {
+
     @Id
-   @GeneratedValue(strategy=GenerationType.AUTO);
-        private String userName;
-        private String userEmail;
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    // Getters and Setters
     public Integer getUserId() {
         return userId;
     }
@@ -21,64 +30,35 @@ public class User {
         this.userId = id;
     }
 
-
-
-        @Column(name = "first_name")
-
-
-       @Column(name = "last_name")
-
-
-
-        public String getUserName() {
-            return userName;
-        }
-
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
-
-        public String getUserEmail() {
-            return userEmail;
-        }
-
-        public void setUserEmail(String userEmail) {
-            this.userEmail = userEmail;
-        }
-
-        public String getFirstName() {
-
-            return firstName;
-        }
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
+    public String getUserName() {
+        return userName;
     }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
+    public String getUserEmail() {
+        return userEmail;
+    }
 
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
