2787e3d3ded1330aec3fd0f2bc364b674afc8973
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-04-23 09:57:33 -0400

    working on controller

--- HerCanvasFeed/src/main/java/rocks/zipcode/Controller/MainController.java
+++ HerCanvasFeed/src/main/java/rocks/zipcode/Controller/MainController.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.*;
 import rocks.zipcode.accessingdatamysql.User;
 import rocks.zipcode.accessingdatamysql.UserRepository;
 
-@Controller
+@RestController
 @RequestMapping( "/demo")
 public class MainController {
     @Autowired
     private UserRepository userRepository;
 
-    public MainController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    //public MainController(UserRepository userRepository) {
+     //   this.userRepository = userRepository;
 
-    @PostMapping(path = "/add")
+
+    @PostMapping(path = "/savedFeed")
     public @ResponseBody String addNewUser (@RequestParam String name , @RequestParam String email,
-      @RequestParam String firstName, @RequestParam String lastName, @RequestParam Integer userId ) {
+       @RequestParam Integer userId ) {
         User n = new User();
 n.setUserName(name);
 n.setUserEmail(email);
