defd1b69ae54cea4f2d18a57089c5b0be111774c
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-04-23 16:29:37 -0400

    working on user controller

--- HerCanvasFeed/src/main/java/rocks/zipcode/Controller/FeedController.java
+++ HerCanvasFeed/src/main/java/rocks/zipcode/Controller/FeedController.java
@@ -1,4 +1,19 @@
 package rocks.zipcode.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rocks.zipcode.accessingdatamysql.FeedRepository;
+
+@RestController
+@RequestMapping("/api/Feed")
 public class FeedController {
+    public FeedController(FeedRepository feedRepository) {
+        this.feedRepository = feedRepository;
+    }
+
+    private FeedRepository feedRepository;
+
+
+
+
 }
--- HerCanvasFeed/src/main/java/rocks/zipcode/Controller/UserController.java
+++ HerCanvasFeed/src/main/java/rocks/zipcode/Controller/UserController.java
@@ -2,21 +2,26 @@ package rocks.zipcode.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rocks.zipcode.accessingdatamysql.User;
 import rocks.zipcode.accessingdatamysql.UserRepository;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/User")
 public class UserController {
-   // @Autowired
-    //    private UserRepository userRepository;
+
+
+    // @Autowired
+       private UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     //    @GetMapping("/{userId}")
     //    public ResponseEntity<User>
     //    getUser (@PathVariable Integer userId) {
     //        // User user = UserRepository.findById(userId).orElseThrow();
+//@PostMapping
+//    public User saveUserWithFeed (@ResponseBody User user){
+//return userRepository.save(user);
+}
 
-    }
